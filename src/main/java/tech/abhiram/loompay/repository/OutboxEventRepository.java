42995636bc3ec069ba0cac648ad3d1897781a00b
Abhirama <abhiram.b@icloud.com>
2026-09-25 21:41:54 +0530

    Fix pageable error

--- src/main/java/tech/abhiram/loompay/repository/OutboxEventRepository.java
+++ src/main/java/tech/abhiram/loompay/repository/OutboxEventRepository.java
@@ -3,7 +3,7 @@ package tech.abhiram.loompay.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.abhiram.loompay.entity.OutboxEvent;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface OutboxEventRepository extends JpaRepository<OutboxEvent, Long> {
